52f7f871b67f19bafc32f5d7e68492196bdaf459
petar <evgenievp@yahoo.com>
2024-10-19 08:21:22 +0300

    elementary graph functionality. Add/remove edges

---
+++ Data Structures/graph.py
@@ -0,0 +1,40 @@
+class Graph:
+    def __init__(self):
+        self.adjacency_list = {}
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+        return False
+
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            try:
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def print_graph(self):
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
+
+
+
+
+my_graph = Graph()
+my_graph.add_edge("A", "B")
+my_graph.add_edge("A", "C")
+my_graph.add_edge("A", "D")
+my_graph.add_edge("B", "C")
+my_graph.add_edge("C", "D")
+my_graph.print_graph()
+
+
+
+
+
